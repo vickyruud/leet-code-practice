f68249e4393b2f9c8b2139e695300ca01261a733
Aprajit Kumar <86376070+vickyruud@users.noreply.github.com>
2022-04-04 12:11:52 -0400

    Time: 94 ms (26.76%), Space: 42.3 MB (29.88%) - LeetHub

---
+++ 58-length-of-last-word/58-length-of-last-word.js
@@ -0,0 +1,13 @@
+/**
+ * @param {string} s
+ * @return {number}
+ */
+const lengthOfLastWord = function(s) {
+    
+    
+    const words = s.trim().split(' ');
+  
+  return words.length > 0 ? words[words.length - 1].length : 0; 
+    
+    
+};
