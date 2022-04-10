b673c14f5b3fbc35a6a5d08e891aafad2e23f092
Aprajit Kumar <86376070+vickyruud@users.noreply.github.com>
2022-04-10 01:28:00 -0400

    Time: 56 ms (97.72%), Space: 42.6 MB (71.25%) - LeetHub

---
+++ 34-find-first-and-last-position-of-element-in-sorted-array/34-find-first-and-last-position-of-element-in-sorted-array.js
@@ -0,0 +1,10 @@
+/**
+ * @param {number[]} nums
+ * @param {number} target
+ * @return {number[]}
+ */
+const searchRange = function(nums, target) {
+  
+  return [nums.indexOf(target), nums.lastIndexOf(target)];
+};
+
