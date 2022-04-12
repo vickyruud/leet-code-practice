aeb4d8445213640265d8a290c661c87e4f9a708a
Aprajit Kumar <86376070+vickyruud@users.noreply.github.com>
2022-04-12 08:20:08 -0400

    Time: 76 ms (55.45%), Space: 42.4 MB (24.28%) - LeetHub

---
+++ 118-pascals-triangle/118-pascals-triangle.js
@@ -0,0 +1,22 @@
+/**
+ * @param {number} numRows
+ * @return {number[][]}
+ */
+const generate = function(numRows) {
+      if (!numRows || numRows <= 0) return [];
+
+  const pascal = [[1]];
+
+  for (let i = 1; i < numRows; i++) {
+    const prevRow = pascal[pascal.length - 1];
+    const shiftLeft = [...prevRow, 0];
+    const shiftRight = [0, ...prevRow];
+    
+    const currentRow = shiftLeft.map((r, i) => r + shiftRight[i]);
+    pascal.push(currentRow);
+  }
+
+  return pascal;
+
+    
+};
