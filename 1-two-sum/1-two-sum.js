fb7ee6c3a2e82004ca22a221513c20f8403c5b23
Aprajit Kumar <86376070+vickyruud@users.noreply.github.com>
2022-05-20 00:09:51 -0400

    Time: 90 ms (69.85%), Space: 44.6 MB (14.20%) - LeetHub

--- 1-two-sum/1-two-sum.js
+++ 1-two-sum/1-two-sum.js
@@ -10,8 +10,8 @@ const twoSum = (nums, target) => {
     nums.forEach((num, i) => {
         const another = target - num
         if(another in object) {
-            result.push(object[another]);
-            result.push(i);            
+            result.push(object[another], i);
+            // result.push(i);            
             
         } else {
             object[nums[i]] = i
