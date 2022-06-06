7aedd434f0edd84ab9032c4355958cebad187b57
Aprajit Kumar <86376070+vickyruud@users.noreply.github.com>
2022-06-06 11:09:34 -0400

    Time: 159 ms (41.67%), Space: 48.9 MB (34.71%) - LeetHub

---
+++ 3-longest-substring-without-repeating-characters/3-longest-substring-without-repeating-characters.js
@@ -0,0 +1,14 @@
+const lengthOfLongestSubstring = function(s) {
+
+  const map = {}
+  let left = 0;
+
+  return s.split('').reduce((max, v, i) => {
+    left = map[v] >= left ? map[v] + 1 : left;
+    map[v] = i;
+    return Math.max(max, i - left + 1);
+  }, 0); 
+    
+    
+};
+
